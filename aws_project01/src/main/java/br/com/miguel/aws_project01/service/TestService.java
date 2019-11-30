017687e30469e2b27b9b6cff54c2dd0bf491eb04
aluno pos <alunopos@local.inatel.br>
2019-11-30 15:10:08 -0200

    Adding aws project

---
+++ aws_project01/src/main/java/br/com/miguel/aws_project01/service/TestService.java
@@ -0,0 +1,10 @@
+package br.com.miguel.aws_project01.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public boolean isUserMatilde(String name) {
+        return "matilde".equals(name);
+    }
+}
